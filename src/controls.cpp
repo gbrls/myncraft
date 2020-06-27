3209e7ffb831eeef677a8b069b0aa2b5eae4b9f5
gbrls <gabrielschneider100@protonmail.com>
2020-06-27 17:36:35 -0300

    Refractored a little bit more.

--- src/controls.cpp
+++ src/controls.cpp
@@ -1,4 +1,43 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "include/controls.hpp"
+
+Camera::Camera (int w, int h) {
+
+	ratio = (float)w/(float)h;
+
+	pos = glm::vec3(0.0, 0.0, -10.0);
+	foward = glm::vec3(0.0, 0.0, -1.0);
+}
+
+glm::mat4 Camera::View(glm::vec3 center) {
+	//                 pos   eye        up
+	return glm::lookAt(pos, pos+foward, glm::vec3(0, 1, 0));
+}
+
+glm::mat4 Camera::Proj() {
+	return glm::perspective(glm::radians(60.0f), ratio, 1.0f, 500.0f);
+}
+
+glm::vec3 Camera::Right() {
+	return glm::normalize(glm::cross(foward, glm::vec3(0, 1, 0)));
+}
+
+void Camera::RotateYaw(float angle) {
+	glm::mat4 m = glm::rotate(glm::mat4(1.0), glm::radians(angle), glm::vec3(0, 1, 0));
+	glm::vec4 tmp = glm::vec4(foward.x, foward.y, foward.z, 1);
+	tmp = m * tmp;
+	foward.x = tmp.x, foward.y = tmp.y, foward.z = tmp.z;
+}
+
+void Camera::RotatePitch(float angle) {
+	glm::mat4 m = glm::rotate(glm::mat4(1.0), glm::radians(angle), Right());
+	glm::vec4 tmp = glm::vec4(foward.x, foward.y, foward.z, 1);
+	tmp = m * tmp;
+	foward.x = tmp.x, foward.y = tmp.y, foward.z = tmp.z;
+}
 
 Controls::Controls () {
 	shift = 0;
--- src/include/controls.hpp
+++ src/include/controls.hpp
@@ -14,40 +14,12 @@ struct Camera {
 	float yaw;
 	float ratio;
 
-	Camera (int w, int h) {
-
-		ratio = (float)w/(float)h;
-
-		pos = glm::vec3(0.0, 0.0, -10.0);
-		foward = glm::vec3(0.0, 0.0, -1.0);
-	}
-
-	glm::mat4 View(glm::vec3 center) {
-		//                 pos   eye        up
-		return glm::lookAt(pos, pos+foward, glm::vec3(0, 1, 0));
-	}
-
-	glm::mat4 Proj() {
-		return glm::perspective(glm::radians(60.0f), ratio, 1.0f, 500.0f);
-	}
-
-	glm::vec3 Right() {
-		return glm::normalize(glm::cross(foward, glm::vec3(0, 1, 0)));
-	}
-
-	void RotateYaw(float angle) {
-		glm::mat4 m = glm::rotate(glm::mat4(1.0), glm::radians(angle), glm::vec3(0, 1, 0));
-		glm::vec4 tmp = glm::vec4(foward.x, foward.y, foward.z, 1);
-		tmp = m * tmp;
-		foward.x = tmp.x, foward.y = tmp.y, foward.z = tmp.z;
-	}
-
-	void RotatePitch(float angle) {
-		glm::mat4 m = glm::rotate(glm::mat4(1.0), glm::radians(angle), Right());
-		glm::vec4 tmp = glm::vec4(foward.x, foward.y, foward.z, 1);
-		tmp = m * tmp;
-		foward.x = tmp.x, foward.y = tmp.y, foward.z = tmp.z;
-	}
+	Camera (int w, int h);
+	glm::mat4 View(glm::vec3 center);
+	glm::mat4 Proj();
+	glm::vec3 Right();
+	void RotateYaw(float angle);
+	void RotatePitch(float angle);
 };
 
 static const long long int BS_MAX = (1LL<<31);
