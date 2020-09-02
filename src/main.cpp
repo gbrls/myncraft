71da9a98c70402e660636eb608a17cca83814188
gbrls <gabrielschneider100@protonmail.com>
2020-09-02 13:12:11 -0300

    code formatation.

--- src/main.cpp
+++ src/main.cpp
@@ -26,16 +26,16 @@
 
 using namespace std;
 
-string read_file(char* filename) {
-	ifstream f(filename);
-	string str;
-	if(f) {
-		ostringstream ss;
-		ss << f.rdbuf();
-		str = ss.str();
-	}
+string read_file(char *filename) {
+    ifstream f(filename);
+    string str;
+    if (f) {
+        ostringstream ss;
+        ss << f.rdbuf();
+        str = ss.str();
+    }
 
-	return str;
+    return str;
 }
 
 
@@ -49,92 +49,92 @@ static glm::vec3 yaw(glm::vec3 orig, float angle) {
 
 int main(int argc, char *argv[]) {
 
-	int w = 1400, h = 900;
+    int w = 1400, h = 900;
 
-	Context ctx = Context(w, h, (char*)"Myncraft");
-	ObjectMesh::LoadDefaults();
+    Context ctx = Context(w, h, (char *) "Myncraft");
+    ObjectMesh::LoadDefaults();
 
-	string vert = read_file((char*)"./src/shaders/cube.vert");
-	string frag = read_file((char*)"./src/shaders/cube.frag");
-	string wire_frag = read_file((char*)"./src/shaders/wireframe.frag");
+    string vert = read_file((char *) "./src/shaders/cube.vert");
+    string frag = read_file((char *) "./src/shaders/cube.frag");
+    string wire_frag = read_file((char *) "./src/shaders/wireframe.frag");
 
-	ctx.loadShader(vert, frag);
+    ctx.loadShader(vert, frag);
 
-	Camera cam = Camera(w,h);
-	Controls ctrl = Controls();
+    Camera cam = Camera(w, h);
+    Controls ctrl = Controls();
 
-	cam.pos = glm::vec3(0,50,0);
+    cam.pos = glm::vec3(0, 50, 0);
 
-	setUniformMatrix(glm::mat4(1.0), ctx.CurShader(), (char*)"model");
-	setUniformMatrix(cam.View(), ctx.CurShader(), (char*)"view");
-	setUniformMatrix(cam.Proj(), ctx.CurShader(), (char*)"proj");
-	setUniformFloat(0.0f, ctx.CurShader(), (char*)"percentage");
+    setUniformMatrix(glm::mat4(1.0), ctx.CurShader(), (char *) "model");
+    setUniformMatrix(cam.View(), ctx.CurShader(), (char *) "view");
+    setUniformMatrix(cam.Proj(), ctx.CurShader(), (char *) "proj");
+    setUniformFloat(0.0f, ctx.CurShader(), (char *) "percentage");
 
-	ctx.loadTexArray((char*)"./assets/block.png", GRASS);
-	ctx.loadTexArray((char*)"./assets/wook_my_0.png", WOOD);
-	ctx.loadTexArray((char*)"./assets/leaves.png", LEAVES);
-	ctx.loadTexArray((char*)"./assets/rock.png", ROCK);
+    ctx.loadTexArray((char *) "./assets/block.png", GRASS);
+    ctx.loadTexArray((char *) "./assets/wook_my_0.png", WOOD);
+    ctx.loadTexArray((char *) "./assets/leaves.png", LEAVES);
+    ctx.loadTexArray((char *) "./assets/rock.png", ROCK);
 
-	World world = World({0, 0, 0}, cam);
+    World world = World({0, 0, 0}, cam);
 
-	vector<std::shared_ptr<ObjectMesh>> meshes;
-	//meshes.push_back(std::make_shared<SunMesh>());
+    vector<std::shared_ptr<ObjectMesh>> meshes;
+    //meshes.push_back(std::make_shared<SunMesh>());
     //meshes.push_back(std::make_shared<ObjectMesh>());
-	auto text = std::make_shared<TextMesh>(10);
-	meshes.push_back(text);
+    auto text = std::make_shared<TextMesh>(10);
+    meshes.push_back(text);
 
-	auto fun = [&]() -> void {
-		glUseProgram(ctx.CurShader());
+    auto fun = [&]() -> void {
+        glUseProgram(ctx.CurShader());
 
-		float t = (float)SDL_GetTicks() * 0.001f;
+        float t = (float) SDL_GetTicks() * 0.001f;
 
-		glm::vec3 sunPos = glm::vec3(sinf(t * 0.01f), -1, cosf(t * 0.02f));
-		setUniformVec3(sunPos, ctx.CurShader(), (char*)"Sun");
+        glm::vec3 sunPos = glm::vec3(sinf(t * 0.01f), -1, cosf(t * 0.02f));
+        setUniformVec3(sunPos, ctx.CurShader(), (char *) "Sun");
 
-		for(auto const& p : world.loaded_chunks) {
-			auto c = p.second;
+        for (auto const &p : world.loaded_chunks) {
+            auto c = p.second;
 
-			bool wire = (world.pos == (XYZ){c->X, c->Y, c->Z});
-			auto cpos = glm::vec3(c->X*32, c->Y*32, c->Z*32);
-            auto v1 = glm::vec3(0,0,1);
+            bool wire = (world.pos == (XYZ) {c->X, c->Y, c->Z});
+            auto cpos = glm::vec3(c->X * 32, c->Y * 32, c->Z * 32);
+            auto v1 = glm::vec3(0, 0, 1);
 
-			auto front = cam.forward;
-			front.y = 0;
-			front = glm::normalize(front);
+            auto front = cam.forward;
+            front.y = 0;
+            front = glm::normalize(front);
 
-			auto left = yaw(front, -65);
+            auto left = yaw(front, -65);
 
-			auto chunkVec = cpos - cam.pos;
-			chunkVec.y = 0;
-			chunkVec = glm::normalize(chunkVec);
+            auto chunkVec = cpos - cam.pos;
+            chunkVec.y = 0;
+            chunkVec = glm::normalize(chunkVec);
 
-			auto angle = glm::acos(glm::dot(chunkVec, front));
+            auto angle = glm::acos(glm::dot(chunkVec, front));
             auto langle = glm::acos(glm::dot(left, front));
 
             auto dist = glm::distance(cam.pos, cpos);
-            if((angle < langle || dist < 100.0f) && dist < 250.0f) c->Draw(wire);
+            if ((angle < langle || dist < 100.0f) && dist < 250.0f) c->Draw(wire);
 
-		}
+        }
 
-		sprintf(text->text, "(%0.2f, %0.2f, %0.2f)", cam.pos.x, cam.pos.y, cam.pos.z);
-		text->Update();
-		for(auto& obj : meshes) {
-			obj->Draw(cam);
-		}
+        sprintf(text->text, "(%0.2f, %0.2f, %0.2f)", cam.pos.x, cam.pos.y, cam.pos.z);
+        text->Update();
+        for (auto &obj : meshes) {
+            obj->Draw(cam);
+        }
 
-		glUseProgram(ctx.CurShader());
-	};
+        glUseProgram(ctx.CurShader());
+    };
 
-	GLenum err;
-	while(ctx.running) {
-		while((err = glGetError()) != GL_NO_ERROR) {
-			printf("Err 0x%x\n", err);
-		}
+    GLenum err;
+    while (ctx.running) {
+        while ((err = glGetError()) != GL_NO_ERROR) {
+            printf("Err 0x%x\n", err);
+        }
 
-		ctx.update(cam, ctrl);
-		world.Update(cam);
-		ctx.draw(cam, fun);
-	}
+        ctx.update(cam, ctrl);
+        world.Update(cam);
+        ctx.draw(cam, fun);
+    }
 
-	return 0;
+    return 0;
 }
